e05234d09a9935bc75507e40ef92b532ce2416bb
Trevor <trevorzack@gmail.com>
2020-03-06 18:07:35 -0500

    pick random should work

--- app/src/main/java/com/example/idontcare/MainActivity.java
+++ app/src/main/java/com/example/idontcare/MainActivity.java
@@ -9,6 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,4 +33,31 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void pickRandom(View view) {
+        String place1 = ((EditText) findViewById(R.id.editText)).getText().toString();
+        int weight1 = Integer.parseInt(((EditText) findViewById(R.id.editText3)).getText().toString());
+        String place2 = ((EditText) findViewById(R.id.editText2)).getText().toString();
+        int weight2 = Integer.parseInt(((EditText) findViewById(R.id.editText6)).getText().toString());
+        String place3 = ((EditText) findViewById(R.id.editText5)).getText().toString();
+        int weight3 = Integer.parseInt(((EditText) findViewById(R.id.editText4)).getText().toString());
+
+        Random random = new Random();
+        int totalWeight = weight1 + weight2 + weight3;
+        int pick = random.nextInt (totalWeight);
+
+        StringBuffer result = new StringBuffer();
+
+        if (pick <= weight1){result.append(place1);}
+        else if (pick <= (weight1 + weight2)){result.append(place2);}
+        else {result.append(place3);}
+
+        ((TextView) findViewById(R.id.finalChoice)).setText(result);
+
+
+
+
+
+
+
+    }
 }
