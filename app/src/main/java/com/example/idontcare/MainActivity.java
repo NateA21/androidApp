86db24b1edd8d962ae16c19c7a5de06395728b0d
NateA21 <nate.athorn@gmail.com>
2020-04-01 18:27:52 -0400

    Added UserDAO, UserDatabase, LoggedInUser

--- app/src/main/java/com/example/idontcare/MainActivity.java
+++ app/src/main/java/com/example/idontcare/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.idontcare;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
