00138ebaf5a4b5d7c83b81369a32d578fc4b6815
Michal Lesniewski <michalklesniewski@gmail.com>
2020-05-05 19:04:44 -0400

    Map gets drawn, but coordinates aren't getting collected.

--- app/src/main/java/com/example/idontcare/MapsActivity.java
+++ app/src/main/java/com/example/idontcare/MapsActivity.java
@@ -4,15 +4,22 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -22,14 +29,17 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    private static final String TAG = "MapsActivity";
+    private GoogleMap mMap; // map object
+    private FusedLocationProviderClient fusedLocationClient; // location service (needed to find last location of device)
+    private static final String TAG = "MapsActivity"; // debug tag
+    private Location userHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         String apiKey = null;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         /* Try to retrieve Google Maps API Key from the manifest */
         try {
@@ -44,12 +54,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (apiKey == null) {
             Log.e(TAG, "apiKey returned null.");
         } else {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        userHome = location;
+                    }
+                }
+            });
+
+            /* Initialized places API to find businesses */
             Places.initialize(getApplicationContext(), apiKey);
             PlacesClient placesClient = Places.createClient(this);
 
             List<Place.Field> placeFields = Collections.singletonList(Place.Field.NAME);
 
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            /* Obtain the SupportMapFragment and get notified when the map is ready to be used. */
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                     .findFragmentById(R.id.map);
             mapFragment.getMapAsync(this);
@@ -69,10 +89,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (userHome == null) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Error finding user's last location.", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
 
         // Add a marker in Sydney and move the camera
+        LatLng home = new LatLng(userHome.getLatitude(), userHome.getLongitude());
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        /*mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(home));
     }
 }
